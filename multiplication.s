1a22ef08fb45589739858d7b67fcb1f454eb3818
AlessandroCanossa <50771606+AlessandroCanossa@users.noreply.github.com>
2019-09-10 14:06:17 +0200

    Update multiplication.s

--- multiplication.s
+++ multiplication.s
@@ -8,6 +8,7 @@ multiplication:
         li      t1, 1       # j = 1
         
         lb      t2, 10(a2)       # t2 = valore della colonna di sparseA a riga I
+        
         InnerLoop:
             
             lb      t3, 10(a1)       # t3 = colonna di sparseB a riga J
@@ -18,20 +19,21 @@ multiplication:
             lb      t3, 5(a1)       # t3 = valore della riga J di sparseB
             
             # calcolo l'indice della matrice C nel quale posizionare il numero calcolato
-            # utilizzo la seguente formula:  I * nrowC - ncolC + J
+            # utilizzo la seguente formula:  row * nrowC - ncolC + col - 1
+            # il meno uno serve per aggiustare gli spostamenti del puntatore
             index_calculation:
-                mul     t4, t4, a3      # parz = I * nrowC
+                mul     t4, t4, a3      # parz = row * nrowC
                 sub     t4, t4, a4      # parz2 = parz - ncolC
-                add     t4, t4, t3      # finale = parz2 + J
+                add     t4, t4, t3      # finale = parz2 + col
                 addi    t4, t4, -1
 
             add     a0, a0, t4      # sposto il puntatore della matrice C all'indice desiderato
 
             lb      t5, 0(a0)       # carico l'eventuale numero presente nella locazione
 
-            lb      t6, 0(a1)
-            lb      t4, 0(a2)
-            mul     t6, t6, t4
+            lb      t6, 0(a1)       # carico il valore della matrice B all'indice desiderato
+            lb      t4, 0(a2)       # eseguo la stessa operazione per la matrice A
+            mul     t6, t6, t4      # moltiplico i due valori
 
             add     t5, t5, t6      # sommo il numero con quello calcolato
 
@@ -40,18 +42,18 @@ multiplication:
             la      a0, sparseC     # reinizializzo il puntatore della matrice C
 
             target: 
-                addi    t1, t1, 1   # j++ nuova iterazione
-                addi    a1, a1, 1   # passiamo alla riga successiva di sparseB
+                addi    t1, t1, 1           # j++ nuova iterazione
+                addi    a1, a1, 1           # passiamo alla riga successiva di sparseB
 
                 ble     t1, a6, InnerLoop   # se t1 è minore del numero di elementi di sparseB
                                             # esegui un nuovo ciclo di InnerLoop
         
         la      a1, sparseB     # resettiamo il puntatore di sparseB
 
-        addi    t0, t0, 1
+        addi    t0, t0, 1       # i++ nuova iterazione
         addi    a2, a2, 1       # passiamo alla successiva riga di sparseA
 
         ble     t0, a5, OuterLoop
 
-    la      a0, matrixC
+    la      a0, matrixC     # resetto il puntatore della matrice C
     ret
